369663da65dbb73abece6ac392bdcb9bc8452eca
Gian Carnero <gian.carnero@gmail.com>
2023-02-26 16:35:43 -0600

    moment installed, changes to main.js, and minor changes to access and meetings.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,80 @@
 // date picker 
-$(function(){
-    $('#datepicker').datepicker();
-  });
+const datepicker = document.querySelector('#datepicker input');
+const timeSlots = document.querySelectorAll('.timeSlot');
 
-document.querySelector('#date').addEventListener('input',updateDate)
+// Add an event listener to the datepicker
+datepicker.addEventListener('change', () => {
+// Get the selected date in UTC format
+const selectedDate = new Date(`${datepicker.value} UTC`);
 
-function updateDate(){
-    console.log('boo')
+// Generate array of dates for the next 7 days (including the selected date)
+const days = [];
+const tzOffset = selectedDate.getTimezoneOffset() * 60 * 1000; 
+for (let i = 0; i < 7; i++) {
+const date = new Date(selectedDate.getTime() + i * 24 * 60 * 60 * 1000 + tzOffset);
+days.push(date);
 }
 
+// Generate array of time slots
+const hours = [];
+const now = new Date();
+const startHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0); 
+const endHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23); 
+  for (let i = 0; i <= 23; i++) {
+    const time = new Date(startHour.getTime() + i * 60 * 60 * 1000);
+    if (time <= endHour || selectedDate > now) {
+      hours.push(time);
+    }
+  }
+
+// Clear the current options for the schedule
+const schedule = document.querySelector('#schedule');
+schedule.innerHTML = '';
+
+// Generate the new options for each day and hour
+days.forEach((day, index) => {
+const dayOfWeek = day.toLocaleDateString('en-US', { weekday: 'long' });
+const dayOfMonth = day.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
+const dayId = dayOfWeek.toLowerCase();
+
+const dayOption = document.createElement('li');
+dayOption.classList.add('day');
+dayOption.setAttribute('id', dayId);
+dayOption.textContent = `${dayOfWeek} ${dayOfMonth}`;
+
+const hourOptions = document.createElement('ol');
+const dayHours = [];
+const startHour = new Date(day.getFullYear(), day.getMonth(), day.getDate(), 0); // updated to represent 12:00AM
+const endHour = new Date(day.getFullYear(), day.getMonth(), day.getDate(), 23);
+
+  for (let i = 0; i <= 23; i++) {
+    const time = new Date(startHour.getTime() + i * 60 * 60 * 1000);
+    if (time <= endHour || day > now) {
+      dayHours.push(time);
+    }
+  }
+
+  dayHours.forEach(hour => {
+    const hourOption = document.createElement('li');
+    hourOption.classList.add('hour');
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('name', 'facilitatorTime');
+    checkbox.setAttribute('class', 'timeSlot');
+    checkbox.setAttribute('value', `${dayOfWeek}, ${hour.toISOString()}`);
+    hourOption.appendChild(checkbox);
+
+    const label = document.createElement('label');
+    label.textContent = hour.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    hourOption.appendChild(label);
+    hourOptions.appendChild(hourOption);
+  });
+
+  dayOption.appendChild(hourOptions);
+  schedule.appendChild(dayOption);
+});
+});
 
 // const deleteBtn = document.querySelectorAll('.del')
 // const todoItem = document.querySelectorAll('span.not')
